85c06c5d7dd0cd58ec7e539504a76d87688f783e
Manthan <manthansoul@gmail.com>
2023-06-10 10:04:28 +0530

    add data for charracters

---
+++ data.js
@@ -0,0 +1,32 @@
+const characterData = {
+  hero: {
+    name: "SHANKS",
+    avatar: "images/shanks.png",
+    health: 60,
+    diceCount: 3,
+    currentDiceScore: [],
+  },
+  mihawk: {
+    name: "MIHAWK",
+    avatar: "images/mihawk.png",
+    health: 30,
+    diceCount: 2,
+    currentDiceScore: [],
+  },
+  crocodile: {
+    name: "CROCODILE",
+    avatar: "images/crocodile.png",
+    health: 20,
+    diceCount: 2,
+    currentDiceScore: [],
+  },
+  buggy: {
+    name: "BUGGY",
+    avatar: "images/buggy.jpg",
+    health: 15,
+    diceCount: 3,
+    currentDiceScore: [],
+  },
+};
+
+export default characterData;
